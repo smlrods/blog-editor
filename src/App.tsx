09bb857a6e23b27b1a5f1e7c0d4325c7faabea19
sml <smlrodrigues.dev@gmail.com>
2023-05-08 15:49:19 -0300

    Set up routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import AllBlogPosts from './components/AllPosts'
+import Login from './components/Login';
+import PostForm from './components/PostForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [token, setToken] = useState<string>();
+
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Login setToken={setToken} />}/>
+          <Route path='/posts' element={<AllBlogPosts token={token}/>}/>
+          <Route path='/posts/:postid' element={<PostForm token={token}/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
