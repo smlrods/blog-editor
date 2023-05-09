b519da366df4b8940e813a21895632590226802f
sml <smlrodrigues.dev@gmail.com>
2023-05-09 13:27:11 -0300

    Change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,20 @@ import './App.css'
 import AllBlogPosts from './components/AllPosts'
 import Login from './components/Login';
 import PostForm from './components/PostForm';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [token, setToken] = useState<string>();
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
           <Route index element={<Login setToken={setToken} />}/>
           <Route path='/posts' element={<AllBlogPosts token={token}/>}/>
           <Route path='/posts/create-post' element={<PostForm token={token}/>}/>
           <Route path='/posts/:postid' element={<PostForm token={token}/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
